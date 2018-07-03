0f5acbbe5cd686a2abae95705a53ea513b0479d3
lokesh yadav <lokesh_rbs@yahoo.com>
2018-07-03 16:47:15 +0530

    assignment

---
+++ index.js
@@ -0,0 +1,30 @@
+//Javascript file
+// variable declaration
+var recipeName = 'Creamy squash linguine'
+var preparationTime = 45;
+var recipeBy = 'Jimmy Cutter';
+var isVegetarian = true;
+var calories = 223.7;
+
+// logging all variables to console
+console.log('Recipe details');
+console.log('--------------');
+console.log('Recipe Name : ' + recipeName);
+console.log('Preparation Time : ' + preparationTime);
+console.log('Recipe By : ' + recipeBy);
+console.log('Is Vegetarian Dish : ' + isVegetarian);
+console.log('Calories : ' + recipeName);
+
+// update variable
+
+preparationTime = 60;
+recipeBy = 'Jimmy Cutter George';
+
+// logging after update
+console.log('Recipe details after update');
+console.log('--------------');
+console.log('Recipe Name : ' + recipeName);
+console.log('Preparation Time : ' + preparationTime);
+console.log('Recipe By : ' + recipeBy);
+console.log('Is Vegetarian Dish : ' + isVegetarian);
+console.log('Calories : ' + recipeName);
